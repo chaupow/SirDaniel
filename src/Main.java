ffa8f337537bd8191155d1f59ab5a783f15b1b1e
Claudi <claudilein@gmail.com>
2013-01-28 11:16:30 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import lejos.nxt.Button;
 
 public class Main {
 
@@ -6,7 +7,8 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Claudia!");
+		System.out.println("Hello World");
+	    Button.waitForAnyPress();
 	}
 
 }

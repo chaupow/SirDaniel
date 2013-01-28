c213881086ccf334a6adde11c8943daadfd3656f
Sophie Laturnus <sophie.laturnus@student.kit.edu>
2013-01-28 14:16:43 +0100

    stop-Methode

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
-import lejos.nxt.Button;
+import lejos.nxt.*;
 
 public class Main {
 
+	static NXTRegulatedMotor right = Motor.A;
+	static NXTRegulatedMotor left = Motor.B;
+	
 	/**
 	 * @param args
 	 */
@@ -10,5 +13,19 @@ public class Main {
 		System.out.println("Hello World!");
 	    Button.waitForAnyPress();
 	}
-
+	
+	
+	/*static void driveForward() {
+		
+		right.setSpeed(900);
+		left.setSpeed(900);
+		
+	}
+	
+	/*Stoppt die Motoren der Kettenraeder*/
+	static void stop() {
+		
+		right.stop();
+		left.stop();
+	}
 }

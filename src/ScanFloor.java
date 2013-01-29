afca7e45a31806690e368eed8c942ab1358c4ae5
Jochen Goertler <lyzrd17@gmail.com>
2013-01-29 13:10:39 +0100

    Behaviors for moving the ultrasonic sensor.

---
+++ src/ScanFloor.java
@@ -0,0 +1,25 @@
+import lejos.nxt.*;
+import lejos.robotics.subsumption.*;
+
+public class ScanFloor implements Behavior {
+	
+	private boolean suppressed = false;	
+	private int floorAngle = 30;
+	
+	public boolean takeControl() {
+		return true;
+	}
+	
+	public void suppress() {
+		suppressed = true;
+	}
+	
+	public void action() {
+		suppressed = false;
+		Motor.C.rotateTo(floorAngle);
+		while( !suppressed )
+	        Thread.yield();
+	}
+	
+
+}
---
+++ src/ScanForward.java
@@ -0,0 +1,25 @@
+import lejos.nxt.*;
+import lejos.robotics.subsumption.*;
+
+public class ScanForward implements Behavior {
+	
+	private boolean suppressed = false;	
+	private int forwardAngle = 180;
+	
+	public boolean takeControl() {
+		return true;
+	}
+	
+	public void suppress() {
+		suppressed = true;
+	}
+	
+	public void action() {
+		suppressed = false;
+		Motor.C.rotateTo(forwardAngle);
+		while( !suppressed )
+	        Thread.yield();
+	}
+	
+
+}

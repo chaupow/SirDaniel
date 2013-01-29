451de019006c7fbed4d5d07e76893070c13f054d
Jochen Goertler <lyzrd17@gmail.com>
2013-01-29 16:53:22 +0100

    Daniel now stops when he detects a void with the ultrasonic sensor.

---
+++ src/DriveForward.java
@@ -0,0 +1,25 @@
+import lejos.nxt.*;
+import lejos.robotics.subsumption.*;
+
+public class DriveForward  implements Behavior {
+   private boolean suppressed = false;
+   
+   public boolean takeControl() {
+      return true;
+   }
+
+   public void suppress() {
+      suppressed = true;
+   }
+
+   public void action() {
+     suppressed = false;
+     Motor.A.forward();
+     Motor.B.forward();
+     while( !suppressed ) {
+    	 Thread.yield();
+     }
+     Motor.A.stop(); // clean up
+     Motor.B.stop();
+   }
+}

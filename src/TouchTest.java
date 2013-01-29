bdd6fa944cd938a6503fe6b22743ac1a4c6c693c
Sophie Laturnus <sophie.laturnus@student.kit.edu>
2013-01-29 20:19:13 +0100

    message

--- src/TouchTest.java
+++ src/TouchTest.java
@@ -9,7 +9,7 @@ public class TouchTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Behavior b1 = new DriveForward(4);
+		Behavior b1 = new DriveForward(3);
 		Behavior b2 = new TouchSensorRight(SensorPort.S2, 60);
 		
 		Behavior [] array = {b1,b2};

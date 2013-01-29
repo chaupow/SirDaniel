ec8f815e87b89e1729fab5161e2d89d7df8fa106
Sophie Laturnus <sophie.laturnus@student.kit.edu>
2013-01-29 13:11:18 +0100

    TouchSensoren action

--- src/TouchSensorLeft.java
+++ src/TouchSensorLeft.java
@@ -1,10 +1,16 @@
 import lejos.nxt.*;
 import lejos.robotics.subsumption.*;
+import lejos.util.Delay;
 
 public class TouchSensorLeft implements Behavior{
 	
 	TouchSensor touch;
 	boolean suppressed = false;
+	
+	public TouchSensorLeft(SensorPort port) {
+		
+		touch = new TouchSensor(port);
+	}
 	
 	public boolean takeControl(){
 		return touch.isPressed();
@@ -12,11 +18,15 @@ public class TouchSensorLeft implements Behavior{
 	
 	public void action(){
 		//anhalten
+		Movement.stop();
 		
 		//rückwärtsfahren
-		
+		Movement.backwards(1);
+		Delay.msDelay(1000);
+		Movement.stop();
 		
 		//Rechtskurve
+		Movement.turn_right(60);
 	}
 	
 	public void suppress(){
--- src/TouchSensorRight.java
+++ src/TouchSensorRight.java
@@ -1,11 +1,16 @@
 
 import lejos.nxt.*;
 import lejos.robotics.subsumption.*;
+import lejos.util.Delay;
 
 public class TouchSensorRight implements Behavior{
 	
 	private TouchSensor touch;
-	private boolean suppressed = false;
+	boolean suppressed = false;
+		
+	public TouchSensorRight(SensorPort port) {
+		touch = new TouchSensor(port);
+	}
 	
 	public boolean takeControl() {
 	
@@ -15,10 +20,17 @@ public class TouchSensorRight implements Behavior{
 	public void action() {
 		
 		//anhalten
+		Movement.stop();
 		
 		//zurueckfahren
+		Movement.backwards(1);
+		Delay.msDelay(1000);
+		Movement.stop();		
 		
 		// nach links drehen
+		
+		Movement.turn_left(60);
+		
 	}
 	
 	public void suppress() {

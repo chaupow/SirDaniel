58c1d484f35f44ce95d50741e39e6e490ef962db
Sophie Laturnus <sophie.laturnus@student.kit.edu>
2013-01-30 11:38:15 +0100

    TouchSensorBridge

---
+++ src/TouchSensorBridge.java
@@ -0,0 +1,25 @@
+
+import lejos.nxt.*;
+import lejos.robotics.subsumption.*;
+
+public class TouchSensorBridge implements Behavior {
+	
+		TouchSensor touch;
+		
+		public TouchSensorBridge(SensorPort touchPort){
+			touch = new TouchSensor(touchPort);
+		}
+		
+		public boolean takeControl(){
+			return (!touch.isPressed());
+		}
+		
+		public void suppress(){
+			
+		}
+		
+		public void action(){
+			Movement.stop();
+			Movement.turn_right(30);
+		}
+}

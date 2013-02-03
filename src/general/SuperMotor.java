fd57ef91b5ccf6e40455d7e41fe7c97700d0e5bc
Sophie Laturnus <sophie.laturnus@student.kit.edu>
2013-02-03 15:39:39 +0100

    SuperMotor ist ansteuerbargit add src/general/SuperMotor.javagit add src/general/SuperMotor.java

--- src/general/SuperMotor.java
+++ src/general/SuperMotor.java
@@ -8,28 +8,41 @@ import lejos.nxt.NXTMotor;
 public final class SuperMotor {
 
 	static NXTMotor motorC = new NXTMotor(MotorPort.C);
+	
+	static int halfRotation = 0; 
 
 	
-	public static void turnTo(int angle) {
+	public static void turnTo(int angle, boolean ImmediateReturn) {
 		
-		//TODO Umrechnung
-		Motor.C.rotateTo(angle);
-	
+		float roundedAngle = (angle/180.0f)*halfRotation;		
+		int currentTacho = motorC.getTachoCount();
+		
+		if (ImmediateReturn){
+			Motor.C.rotate((int)(roundedAngle-currentTacho), true);
+		}else {
+			Motor.C.rotateTo((int)roundedAngle);
+		}
+		
 		}
 	
+	public static void calibrate(){
+		calibrateRight();
+		calibrateLeft();
+	}
 	
-	public static void calibrate() {
+	/**Faehrt den Schwenkarm nach rechts und setzt dort den Tacho auf Null**/
+	private static void calibrateRight() {
 		
 		long lastTime = System.currentTimeMillis();
-		
-		int power = 30;
+		long timePassed;
+		int power = 20;
 		
 		boolean stalled = false;
 		int tachoCountC = -1;
 
-		
+		//Fahre nachrechts bis du anstoesst und setze diesen wert auf null
 		while (!stalled) {
-			long timePassed = System.currentTimeMillis() - lastTime;
+			timePassed = System.currentTimeMillis() - lastTime;
 			
 		if( timePassed > 100) { 
 			motorC.setPower(-power);
@@ -50,8 +63,45 @@ public final class SuperMotor {
 		}
 
 		}
+		LCD.drawString("I reseted the TachoCount",1,1);
+		motorC.resetTachoCount();
+		motorC.setPower(0);
 		
-		Motor.C.resetTachoCount();
+	}
+	
+	/**Faehrt den Schwenkarm nach links und speichert dessen TachoCount**/
+	private static void calibrateLeft(){
+		long lastTime = System.currentTimeMillis();
+		long timePassed;
+		int power = 20;
+		boolean stalled = false;
+		int tachoCountC = -100;
+
+		//fahre so lange nach links, bis du anstoesst und setze diesen wert auf 180;
+		while (!stalled) {
+			timePassed = System.currentTimeMillis() - lastTime;
+			
+			if( timePassed > 100) { 
+				motorC.setPower(power);
+			
+				int currentCountC = motorC.getTachoCount();
+		
+				if (currentCountC == tachoCountC ) {
+					stalled = true;
+					LCD.drawString("I stalled...", 0, 6);
+					LCD.refresh();
+				} else {
+					LCD.drawString("I not stalled...", 0, 6);
+					LCD.refresh();
+				}	
+			
+				tachoCountC = currentCountC;
+				lastTime = System.currentTimeMillis();
+			}
+		}
+		LCD.drawString("I set the halfRotation", 1, 2);
+		halfRotation = motorC.getTachoCount();
+
 	}
 	
 	

b462b02142dbc69a4f9419832ae1a6b7041408ac
Sophie Laturnus <sophie.laturnus@student.kit.edu>
2013-02-05 13:41:59 +0100

    Solly

--- src/p0_Race/P0_Correct.java
+++ src/p0_Race/P0_Correct.java
@@ -1,5 +1,46 @@
 package p0_Race;
 
-public class P0_Correct {
+import general.Calibration;
+import general.Movement;
+import lejos.nxt.LCD;
+import lejos.nxt.UltrasonicSensor;
+import lejos.robotics.subsumption.Behavior;
+
+public class P0_Correct implements Behavior{
+	
+	UltrasonicSensor sonic;
+	int min_dist = 0;
+	int dist = 0;
+	
+	public boolean takeControl() {
+		
+		dist = sonic.getDistance() - min_dist;
+
+		return (Calibration.race && dist != 0);
+	}
+
+	@Override
+	public void action() {
+		
+		if (dist < 0) {
+
+			LCD.drawString("Dist: " + dist, 0, 0);
+			LCD.drawString("Steering left ", 0, 1);
+			Movement.getInstance().steer(25, -2*dist, true);
+		} else {
+			LCD.drawString("Dist: " + dist, 0, 4);
+			LCD.drawString("Steering right ", 0, 5);
+			
+			Movement.getInstance().steer(-25, -2*dist, true);
+		}
+	
+	}
+
+	@Override
+	public void suppress() {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 }

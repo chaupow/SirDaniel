45ab9c5f07b9577710155441a26f131c033f7bba
Jochen Goertler <lyzrd17@gmail.com>
2013-02-05 16:01:44 +0100

    BarcodeReader. evtl noch mit getLightValue() implementieren.

---
+++ src/general/BarcodeReader.java
@@ -0,0 +1,64 @@
+package general;
+
+public class BarcodeReader implements Runnable {
+	
+	private int TIME_FOR_SUFFIX = 500;
+	private int THRESHOLD = 100;
+	
+	@Override
+	public void run() {
+	
+		System.out.println("Looking for Barcodes.");
+		
+		// always look for barcodes
+		while(true) {
+			
+			boolean rising = true;
+			int min = 1000;
+			int max = 0;
+			
+			int currentValue;
+			long currentTimestamp = System.currentTimeMillis();
+			long lastTimestamp = System.currentTimeMillis();
+			int lineCount = 0;
+
+			while (currentTimestamp - lastTimestamp < TIME_FOR_SUFFIX) {
+				
+				// TODO use getLightValue() instead?
+				currentValue = SensorCache.getInstance().light.getNormalizedLightValue();
+				currentTimestamp = System.currentTimeMillis();
+				if (rising) {
+					if (currentValue >= max) {
+						max = currentValue;
+					} else {
+						rising = false;
+						min = max;
+					}			
+				} else {
+					if (currentValue <= min) {
+						min = currentValue;
+					} else {
+						rising = true;
+						//System.out.println("T:" + (max-min));
+						if (max-min > THRESHOLD) {
+							lastTimestamp = currentTimestamp;
+							lineCount++;
+						}
+						max = min;
+					}
+				}		
+			}
+			
+			if (lineCount >= 3) {
+				startBehavior(lineCount);
+			}
+		}
+	}
+		
+	
+	private void startBehavior(int number) {
+		// TODO do cool stuff here
+		System.out.println("Called Behavior: " + number);
+	}
+
+}

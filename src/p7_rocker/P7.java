1dd60aeb92c8b93f11ee149c023ef9484a496203
Claudi <claudilein@gmail.com>
2013-02-06 05:41:35 +0100

    Rocker um Linie erweitert.

--- src/p7_rocker/P7.java
+++ src/p7_rocker/P7.java
@@ -1,5 +1,6 @@
 package p7_rocker;
 
+import p6_slider.P6;
 import lejos.nxt.SensorPort;
 import lejos.nxt.UltrasonicSensor;
 import lejos.robotics.subsumption.Behavior;
@@ -10,6 +11,15 @@ public class P7 {
 	
 	SirDanielArbitrator arby;
 	UltrasonicSensor sonic = new UltrasonicSensor(SensorPort.S3);
+	
+	private static P7 instance = null;
+
+	public static P7 getInstance() {
+		if(instance == null) {
+			instance = new P7();
+		}
+		return instance;
+	}
 	
 	public void start() {
 		

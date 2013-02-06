9be49d8d56a647d7d38fbdded6dd31378d49bfff
Huyen Chau Nguyen <Chau@scc-wkit-clx-235-88.scc.kit.edu>
2013-02-06 07:00:55 +0100

    ColorButton funktioniert! Soll noch mit Labyrinth umgesetzt werden

--- src/p8_colorbuttons/ColorButtons.java
+++ src/p8_colorbuttons/ColorButtons.java
@@ -1,7 +1,5 @@
 package p8_colorbuttons;
 
-import p8_Line.P8_Line;
-import general.ClaudisMain;
 import general.Movement;
 import general.SirDanielArbitrator;
 import general.SuperMotor;
@@ -16,23 +14,26 @@ public class ColorButtons {
 	public int [] average = new int[3];
 	public int blackLines = -1;
 	ColorGateControl gate = new ColorGateControl();
-	SirDanielArbitrator arby;
-	static ColorButtons cb = null;
 	boolean boss = false;
+	SirDanielArbitrator arby;
 	
-	public static ColorButtons getInstance(){
-		if (cb == null) {
-			cb = new ColorButtons();
-		}
-		return cb;
-	}
-	
-	private ColorButtons (){
-		
-	}
-	
+//	public void run() {
+//		Behavior talkToGate = new P8_TalkToGate(this, gate);
+//		Behavior findColor = new P8_FindColor(this);
+//		Behavior defineColors = new P8_DefineColors(this);
+//		Behavior driveOver = new P8_DriveOverColors();
+//		Behavior setColors = new P8_SetColors(this);
+//		Behavior pushButton = new P8_PushButton(this);
+//		Behavior throughGate = new P8_DriveThroughGate(this);
+//
+//		SuperMotor.turnTo(90, false);
+//		Behavior [] bArray = {defineColors, driveOver, setColors, findColor, pushButton, throughGate, talkToGate};
+//		arby = new SirDanielArbitrator(bArray, true);
+//		Thread t = new Thread(arby);
+//		t.start();
+//	}
+
 	public void start() {
-		
 		Behavior talkToGate = new P8_TalkToGate(this, gate);
 		Behavior findColor = new P8_FindColor(this);
 		Behavior defineColors = new P8_DefineColors(this);
@@ -40,24 +41,24 @@ public class ColorButtons {
 		Behavior setColors = new P8_SetColors(this);
 		Behavior pushButton = new P8_PushButton(this);
 		Behavior throughGate = new P8_DriveThroughGate(this);
-		Behavior boss = new P8_BOSS();
-		
+		Behavior boss = new P8_BOSS(this);
+
 		SuperMotor.turnTo(90, false);
 		Behavior [] bArray = {defineColors, driveOver, setColors, findColor, pushButton, throughGate, talkToGate, boss};
 		arby = new SirDanielArbitrator(bArray, true);
 		Thread t = new Thread(arby);
-		System.out.println("I will start now");
 		t.start();
 	}
 	
-	public void stop(){
+	public void stop() {
 		arby.stop();
 	}
+	
 	
 	public static void main (String[] args) {
 		SuperMotor.calibrate();
 		Button.waitForAnyPress();
-		System.out.println("I will get Instance now");
-		ColorButtons.getInstance().start();
+		ColorButtons cb = new ColorButtons();
+		cb.start();
 	} 
 }
